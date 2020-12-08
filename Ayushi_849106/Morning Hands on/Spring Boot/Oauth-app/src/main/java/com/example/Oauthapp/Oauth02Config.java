b8e226de47cdcf3bf785ce5a8a1b9547c3d1e93c
Ayushi1849 <ayushi1849@gmail.com>
2020-12-09 00:50:46 +0530

    Oauth2 exercise

---
+++ Ayushi_849106/Morning Hands on/Spring Boot/Oauth-app/src/main/java/com/example/Oauthapp/Oauth02Config.java
@@ -0,0 +1,36 @@
+package com.example.Oauthapp;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+
+
+public class Oauth02Config extends AuthorizationServerConfigurerAdapter {
+
+	@Autowired
+	private AuthenticationManager authenticationManager;
+	
+	@Autowired
+	private UserDetailsService userDetailsService;
+
+	@Override
+	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+		// you can check the application details either in database or in-memory
+		clients.inMemory()
+		.withClient("my-client")
+		.secret("{noop}secretpassword")
+		.authorizedGrantTypes("password")
+		.scopes("webclient", "mobileclient");
+	}
+
+	@Override
+	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+		endpoints.authenticationManager(authenticationManager).userDetailsService(userDetailsService);
+	}
+	
+} 
